c092ea5e9722cbb0baddf400d363ab62d2431cb8
RachelChen <iceage91@gmail.com>
2021-12-06 20:37:22 +0800

    add followship-likeship api

--- src/views/User.vue
+++ src/views/User.vue
@@ -16,7 +16,9 @@
             @click="$router.back()"
           />
           <div class="header-info">
-            <p>John Doe</p>
+            <!-- <p>John Doe</p>
+            <p>25 <span>推文</span></p> -->
+            <p>{{ getCurrentUser.name }}</p>
             <p>25 <span>推文</span></p>
           </div>
         </div>
@@ -48,13 +50,12 @@ import Tab from "../modules/user/Tab.vue";
 import Comments from "../modules/user/Comments.vue";
 import UserEditModal from "../modules/user/UserEditModal.vue";
 
-// import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
 
-// import {
-//   SET_TWEETS_FILTER_TYPE,
-//   GET_ONE_USER_TWEETS,
-//   SET_ONE_USER_TWEETS,
-// } from "../store/store-types";
+import {
+  GET_CURRENT_USER,
+  // SET_ONE_USER_TWEETS,
+} from "../store/store-types";
 export default {
   components: {
     Popular,
@@ -85,6 +86,11 @@ export default {
     return {
       showReplyModal: false,
     };
+  },
+  computed: {
+    ...mapGetters({
+      getCurrentUser: GET_CURRENT_USER,
+    }),
   },
   // created() {
   //   this.setTweetsFilterType({
